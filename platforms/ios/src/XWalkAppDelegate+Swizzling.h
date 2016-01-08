f5a41948ea85317086da4d500901f9bbb0ba0f74
Jonathan Dong <jonathan.dong@intel.com>
2016-01-08 10:28:37 +0800

    remove files.

--- platforms/ios/src/XWalkAppDelegate+Swizzling.h
+++
@@ -1,9 +0,0 @@
-// Copyright (c) 2015 Intel Corporation. All rights reserved.
-// Use of this source code is governed by a Apache V2.0 license that can be
-// found in the LICENSE file.
-
-#include "AppDelegate.h"
-
-@interface AppDelegate (Swizzling)
-
-@end
